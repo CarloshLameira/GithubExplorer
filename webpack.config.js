e9f4f2f1f4304257314c09285089c1801db4d97b
CarloshLameira <carloshlameira@hotmail.com>
2022-02-17 21:29:03 -0300

    ambiente finalizado

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production';//cria uma variavel para dizer se estamos no desenvovimento ou producao
+
 module.exports =
 {
+        mode: isDevelopment ? 'development':'production',
+        devtool:isDevelopment ? 'eval-source-map': 'source-map',//varios tipos de source map para conseguir entender melhor o codigo em js puro
         entry: path.resolve(__dirname, 'src','index.jsx'), //qual o arquivo inicial da nossa aplicacao
         output: { //onde vai o resultado da nossa aplicacao
                 path: path.resolve(__dirname,'dist'),
@@ -9,6 +15,14 @@ module.exports =
         resolve:{
                 extensions: ['.js', '.jsx'] //que tipos de arquivos nossa aplicacao vai receber
                 },
+                devServer: {//pra n precisar fazer toda hora o yarn webpack
+                        static: path.resolve(__dirname, 'public'),
+                      },
+                plugins: [//monstrar sempre onde esta a aplicacao
+                        new HtmlWebpackPlugin({
+                                template: path.resolve(__dirname,'public','index.html')
+                        })
+                ],
         module: //como a nossa aplicacao vai se comportarquando estivermos importando cada tipo de arquivo
         {
                 rules:
@@ -17,6 +31,11 @@ module.exports =
                         test: /\.jsx$/, //cifrao diz que o arquivo deve terminar em ~ 
                         exclude: /node_modules/,//o mode faz o trabalho que o webpack faria entao eu tiro essa funcionalidade
                         use: 'babel-loader',//integracao entre o babel e webpack
+                        },
+                        {
+                        test: /\.scss$/, //cifrao diz que o arquivo deve terminar em ~ 
+                        exclude: /node_modules/,//o mode faz o trabalho que o webpack faria entao eu tiro essa funcionalidade
+                        use: ['style-loader', 'css-loader', 'sass-loader'] //integracao entre o babel e webpack
                         },
 
                 ]
